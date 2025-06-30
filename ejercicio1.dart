68c54ccbac4ea1ed4be96aeeaef75e14dcf60657
milena-dev-code <milepeletaymartinez@gmail.com>
2025-06-30 17:11:00 -0300

    resolviendo ej1

--- ejercicio1.dart
+++ ejercicio1.dart
@@ -5,3 +5,8 @@ List<int> numeros = [5, 10, 15, 20, 25];
 Imprime cada elemento de la lista seguido de la palabra “es un número” usando
 forEach().
  */
+
+void main() {
+  List<int> numeros = [5, 10, 15, 20, 25];
+  numeros.forEach((num) => print("${num} es un numero"));
+}
