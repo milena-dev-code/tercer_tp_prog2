dd1b0be4b22daffe995b65d9a909bdc2b9f62453
milena-dev-code <milepeletaymartinez@gmail.com>
2025-06-30 17:39:12 -0300

    resolviendo ej9

--- ejercicio9.dart
+++ ejercicio9.dart
@@ -3,3 +3,11 @@ Omitir los 2 primeros elementos (skip)
 Crea una nueva lista que omita los 2 primeros elementos de numeros usando skip().
 Imprime la nueva lista.
 */
+
+void main() {
+  List<int> numeros = [5, 10, 15, 20, 25];
+
+  List<int> menosDosPrimeros = numeros.skip(2).toList();
+
+  print(menosDosPrimeros);
+}
