bf43707de816dfaf5649c7e28c19b6d7faed5cc7
milena-dev-code <milepeletaymartinez@gmail.com>
2025-06-30 17:18:05 -0300

    resolviendo ej3

--- ejercicio3.dart
+++ ejercicio3.dart
@@ -3,3 +3,9 @@ Filtrar pares (where)
 Usando la misma lista numeros, crea una nueva lista que contenga solo los números
 pares utilizando where().
  */
+void main() {
+  List<int> numeros = [5, 10, 15, 20, 25];
+  List<int> numerosPares = numeros.where((num) => num % 2 == 0).toList();
+
+  print(numerosPares);
+}
