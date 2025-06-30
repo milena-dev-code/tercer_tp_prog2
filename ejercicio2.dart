4a8341bc625a625c0272285ad2c14663aa26b905
milena-dev-code <milepeletaymartinez@gmail.com>
2025-06-30 17:15:12 -0300

    resolviendo ej2

--- ejercicio2.dart
+++ ejercicio2.dart
@@ -3,3 +3,9 @@ Duplicar elementos (map)
 A partir de la lista anterior, genera una nueva lista que contenga el doble de cada
 elemento usando map(). Imprime la lista resultante.
  */
+
+void main() {
+  List<int> numeros = [5, 10, 15, 20, 25];
+  List<int> numerosDuplicados = numeros.map((num) => num * 2).toList();
+  print(numerosDuplicados);
+}
