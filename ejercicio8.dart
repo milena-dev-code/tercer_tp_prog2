ce4a58ed708e9fc6bca00d14101c0c8b661ca62a
milena-dev-code <milepeletaymartinez@gmail.com>
2025-06-30 17:36:51 -0300

    resolviendo ej8

--- ejercicio8.dart
+++ ejercicio8.dart
@@ -3,3 +3,11 @@ Tomar los 3 primeros elementos (take)
 Crea una nueva lista que contenga los 3 primeros elementos de numeros usando
 take(). Imprime la nueva lista.
 */
+
+void main() {
+  List<int> numeros = [5, 10, 15, 20, 25];
+
+  List<int> tresPrimeros = numeros.take(3).toList();
+
+  print(tresPrimeros);
+}
