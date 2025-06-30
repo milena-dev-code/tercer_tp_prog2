681e5056d54c5d589a72de37b3cc03c84ac88cb9
milena-dev-code <milepeletaymartinez@gmail.com>
2025-06-30 17:20:51 -0300

    resolviendo ej4

--- ejercicio4.dart
+++ ejercicio4.dart
@@ -2,3 +2,10 @@
 Sumar todos los elementos (reduce)
 Suma todos los elementos de numeros usando reduce() y muestra el resultado.
  */
+
+void main() {
+  List<int> numeros = [5, 10, 15, 20, 25];
+  int suma = numeros.reduce((num1, num2) => num1 + num2);
+
+  print(suma);
+}
