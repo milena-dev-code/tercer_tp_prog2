bdcc630748959d51d254500d1323e00624d35703
milena-dev-code <milepeletaymartinez@gmail.com>
2025-06-30 18:00:49 -0300

    resolviendo ej10

--- ejercicio10.dart
+++ ejercicio10.dart
@@ -4,3 +4,12 @@ Primero, filtra los números pares de numeros usando where().
 Luego, usa map() para elevar esos números al cuadrado.
 Imprime la lista final.
 */
+import 'dart:math';
+void main() {
+  List<int> numeros = [5, 10, 15, 20, 25];
+
+  List<num> paresAlCuadrado = numeros.where((numero) => numero % 2
+== 0).toList().map((numeroPar) => pow(numeroPar,2)).toList();
+
+  print(paresAlCuadrado);
+}
