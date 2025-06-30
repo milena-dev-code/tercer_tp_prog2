b1807feea2cccb83105c0e26b4df24a7a1175617
milena-dev-code <milepeletaymartinez@gmail.com>
2025-06-30 17:27:31 -0300

    resolviendo ej5

--- ejercicio5.dart
+++ ejercicio5.dart
@@ -3,3 +3,12 @@
 Calcula el producto de todos los elementos de numeros usando fold() con valor inicial
 1. Imprime el resultado.
 */
+
+void main() {
+  List<int> numeros = [5, 10, 15, 20, 25];
+  //iniciar con 1 y no con 0 
+  //ya que todo numero multiplicado por 0 da 0
+  int producto = numeros.fold(1, (num1, num2) => num1 * num2);
+
+  print(producto);
+}
