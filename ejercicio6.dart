92e48c40126f1f5aafbe6e045ca460e42b1c249b
milena-dev-code <milepeletaymartinez@gmail.com>
2025-06-30 17:32:11 -0300

    resolviendo ej6

--- ejercicio6.dart
+++ ejercicio6.dart
@@ -3,3 +3,8 @@ Verificar si algún número es mayor a 20 (any)
 Usa any() para verificar si algún número en la lista numeros es mayor a 20. Imprime
 true o false.
 */
+
+void main() {
+  List<int> numeros = [5, 10, 15, 20, 25];
+  print(numeros.any((num) => num > 20));
+}
