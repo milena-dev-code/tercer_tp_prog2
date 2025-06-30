80b0f6dbc710a91352253042adb3a424e76ce55e
milena-dev-code <milepeletaymartinez@gmail.com>
2025-06-30 17:34:28 -0300

    resolviendo ej7

--- ejercicio7.dart
+++ ejercicio7.dart
@@ -3,3 +3,8 @@ Verificar si todos los números son mayores que 0 (every)
 Usa every() para comprobar si todos los elementos son mayores que 0. Imprime el
 resultado.
  */
+
+void main() {
+  List<int> numeros = [5, 10, 15, 20, 25];
+  print(numeros.every((num) => num > 0));
+}
